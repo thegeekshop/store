5f218ca27df90b8bfb0664970337d78f02342252
thegeekshop <thegeek.shop0@gmail.com>
2025-08-20 20:57:53 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC-8wLcmLaLs87ZZ2WjgnmmILiOfcnINg8",
+  apiKey: "${{ secrets.API_KEY }}",
   authDomain: "geekshopbd.firebaseapp.com",
   projectId: "geekshopbd",
   storageBucket: "geekshopbd.firebasestorage.app",
@@ -12,6 +12,7 @@ export const BKASH_NUMBER = '01960788862 (Personal)'; // Replace with your Bkash
 export const COD_NUMBER = '01960788862 (Personal)'; // Replace with your COD contact number
 
 export const DELIVERY_FEE = 70; // Default delivery fee
+
 
 
 
